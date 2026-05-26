059d3ed6a315323874c022361312729df31822b6
Mancave-Industries <markymid@me.com>
2026-05-26 18:40:35 +0100

    Update suspects.js

--- data/suspects.js
+++ data/suspects.js
@@ -1,1 +1,419 @@
+window.CLUEDLE_DATA = window.CLUEDLE_DATA || {};
+window.CLUEDLE_DATA.suspects = [
+  {
+    "id": "bianca_frost",
+    "name": "Bianca Frost",
+    "image": "assets/suspects/bianca_frost.JPG",
+    "visibleTraits": [
+      "female",
+      "black",
+      "dark hair",
+      "long hair",
+      "wavy hair",
+      "no glasses",
+      "athletic",
+      "fashionable"
+    ],
+    "hiddenTraits": [
+      "wealthy",
+      "influencer",
+      "extrovert"
+    ]
+  },
+  {
+    "id": "cleo_saint",
+    "name": "Cleo Saint",
+    "image": "assets/suspects/cleo_saint.JPG",
+    "visibleTraits": [
+      "female",
+      "medium skin",
+      "dark hair",
+      "wavy hair",
+      "glasses",
+      "earrings",
+      "fashionable"
+    ],
+    "hiddenTraits": [
+      "wealthy"
+    ]
+  },
+  {
+    "id": "dorian_luxe",
+    "name": "Dorian Luxe",
+    "image": "assets/suspects/dorian_luxe.JPG",
+    "visibleTraits": [
+      "male",
+      "black",
+      "dark hair",
+      "glasses",
+      "beard",
+      "fashionable"
+    ],
+    "hiddenTraits": [
+      "wealthy"
+    ]
+  },
+  {
+    "id": "gideon_pryce",
+    "name": "Gideon Pryce",
+    "image": "assets/suspects/gideon_pryce.JPG",
+    "visibleTraits": [
+      "male",
+      "dark hair",
+      "no glasses",
+      "fashionable"
+    ],
+    "hiddenTraits": [
+      "wealthy",
+      "aggressive",
+      "cold"
+    ]
+  },
+  {
+    "id": "harvey_slate",
+    "name": "Harvey Slate",
+    "image": "assets/suspects/harvey_slate.JPG",
+    "visibleTraits": [
+      "male",
+      "beard",
+      "no glasses",
+      "fashionable"
+    ],
+    "hiddenTraits": [
+      "wealthy",
+      "aggressive"
+    ]
+  },
+  {
+    "id": "india_gold",
+    "name": "India Gold",
+    "image": "assets/suspects/India_gold.JPG",
+    "visibleTraits": [
+      "female",
+      "dark hair",
+      "long hair",
+      "wavy hair",
+      "no glasses",
+      "earrings",
+      "fashionable"
+    ],
+    "hiddenTraits": [
+      "wealthy",
+      "influencer",
+      "extrovert"
+    ]
+  },
+  {
+    "id": "jaxon_vale",
+    "name": "Jaxon Vale",
+    "image": "assets/suspects/jaxon_vale.JPG",
+    "visibleTraits": [
+      "male",
+      "medium skin",
+      "dark hair",
+      "beard",
+      "no glasses",
+      "athletic",
+      "fashionable"
+    ],
+    "hiddenTraits": [
+      "wealthy",
+      "tech"
+    ]
+  },
+  {
+    "id": "milo_vex",
+    "name": "Milo Vex",
+    "image": "assets/suspects/milo-vale.JPG",
+    "visibleTraits": [
+      "male",
+      "dark hair",
+      "glasses"
+    ],
+    "hiddenTraits": [
+      "tech",
+      "introvert",
+      "intelligent"
+    ]
+  },
+  {
+    "id": "nova_wilde",
+    "name": "Nova Wilde",
+    "image": "assets/suspects/nova_wilde.JPG",
+    "visibleTraits": [
+      "female",
+      "dark hair",
+      "wavy hair",
+      "glasses",
+      "earrings"
+    ],
+    "hiddenTraits": [
+      "creative",
+      "introvert",
+      "calm"
+    ]
+  },
+  {
+    "id": "otis_blank",
+    "name": "Otis Blank",
+    "image": "assets/suspects/otis_blank.JPG",
+    "visibleTraits": [
+      "male",
+      "glasses"
+    ],
+    "hiddenTraits": [
+      "wealthy",
+      "tech",
+      "cold",
+      "calm"
+    ]
+  },
+  {
+    "id": "piper_bloom",
+    "name": "Piper Bloom",
+    "image": "assets/suspects/piper_bloom.JPG",
+    "visibleTraits": [
+      "female",
+      "blonde hair",
+      "wavy hair",
+      "no glasses",
+      "earrings",
+      "fashionable"
+    ],
+    "hiddenTraits": [
+      "calm",
+      "wellness"
+    ]
+  },
+  {
+    "id": "rex_branson",
+    "name": "Rex Branson",
+    "image": "assets/suspects/rex_branson.JPG",
+    "visibleTraits": [
+      "male",
+      "dark hair",
+      "beard",
+      "no glasses",
+      "athletic"
+    ],
+    "hiddenTraits": [
+      "aggressive",
+      "extrovert"
+    ]
+  },
+  {
+    "id": "saffron_skye",
+    "name": "Saffron Skye",
+    "image": "assets/suspects/saffron_skye.JPG",
+    "visibleTraits": [
+      "female",
+      "blonde hair",
+      "long hair",
+      "wavy hair",
+      "glasses",
+      "fashionable"
+    ],
+    "hiddenTraits": [
+      "wealthy",
+      "calm",
+      "wellness"
+    ]
+  },
+  {
+    "id": "sebastian_drift",
+    "name": "Sebastian Drift",
+    "image": "assets/suspects/sebastian_drift.JPG",
+    "visibleTraits": [
+      "male",
+      "medium skin",
+      "dark hair",
+      "glasses",
+      "athletic"
+    ],
+    "hiddenTraits": [
+      "tech"
+    ]
+  },
+  {
+    "id": "tilly_chrome",
+    "name": "Tilly Chrome",
+    "image": "assets/suspects/tilly_chrome.JPG",
+    "visibleTraits": [
+      "female",
+      "dark hair",
+      "fashionable"
+    ],
+    "hiddenTraits": [
+      "creative",
+      "extrovert"
+    ]
+  },
+  {
+    "id": "velvet_kane",
+    "name": "Velvet Kane",
+    "image": "assets/suspects/velvet_kane.JPG",
+    "visibleTraits": [
+      "female",
+      "dark hair",
+      "glasses"
+    ],
+    "hiddenTraits": [
+      "wealthy",
+      "intelligent"
+    ]
+  }
+];
+
+window.CLUEDLE_DATA.suspectTraitMatrix = {
+  "glasses": {
+    "type": "visible",
+    "frequency": 8,
+    "tier": "common",
+    "value": 2
+  },
+  "no glasses": {
+    "type": "visible",
+    "frequency": 8,
+    "tier": "common",
+    "value": 2
+  },
+  "dark hair": {
+    "type": "visible",
+    "frequency": 9,
+    "tier": "common",
+    "value": 2
+  },
+  "wealthy": {
+    "type": "hidden",
+    "frequency": 10,
+    "tier": "common",
+    "value": 2
+  },
+  "fashionable": {
+    "type": "visible",
+    "frequency": 8,
+    "tier": "common",
+    "value": 2
+  },
+  "wavy hair": {
+    "type": "visible",
+    "frequency": 6,
+    "tier": "common",
+    "value": 2
+  },
+  "female": {
+    "type": "visible",
+    "frequency": 8,
+    "tier": "common",
+    "value": 2
+  },
+  "male": {
+    "type": "visible",
+    "frequency": 8,
+    "tier": "common",
+    "value": 2
+  },
+  "earrings": {
+    "type": "visible",
+    "frequency": 4,
+    "tier": "uncommon",
+    "value": 4
+  },
+  "athletic": {
+    "type": "visible",
+    "frequency": 4,
+    "tier": "uncommon",
+    "value": 4
+  },
+  "tech": {
+    "type": "hidden",
+    "frequency": 4,
+    "tier": "uncommon",
+    "value": 4
+  },
+  "calm": {
+    "type": "hidden",
+    "frequency": 4,
+    "tier": "uncommon",
+    "value": 4
+  },
+  "aggressive": {
+    "type": "hidden",
+    "frequency": 4,
+    "tier": "uncommon",
+    "value": 4
+  },
+  "influencer": {
+    "type": "hidden",
+    "frequency": 3,
+    "tier": "uncommon",
+    "value": 4
+  },
+  "beard": {
+    "type": "visible",
+    "frequency": 3,
+    "tier": "uncommon",
+    "value": 4
+  },
+  "blonde hair": {
+    "type": "visible",
+    "frequency": 3,
+    "tier": "uncommon",
+    "value": 4
+  },
+  "introvert": {
+    "type": "hidden",
+    "frequency": 3,
+    "tier": "uncommon",
+    "value": 4
+  },
+  "extrovert": {
+    "type": "hidden",
+    "frequency": 3,
+    "tier": "uncommon",
+    "value": 4
+  },
+  "medium skin": {
+    "type": "visible",
+    "frequency": 2,
+    "tier": "rare",
+    "value": 7
+  },
+  "black": {
+    "type": "visible",
+    "frequency": 2,
+    "tier": "rare",
+    "value": 7
+  },
+  "creative": {
+    "type": "hidden",
+    "frequency": 2,
+    "tier": "rare",
+    "value": 7
+  },
+  "cold": {
+    "type": "hidden",
+    "frequency": 2,
+    "tier": "rare",
+    "value": 7
+  },
+  "intelligent": {
+    "type": "hidden",
+    "frequency": 2,
+    "tier": "rare",
+    "value": 7
+  },
+  "wellness": {
+    "type": "hidden",
+    "frequency": 2,
+    "tier": "rare",
+    "value": 7
+  },
+  "long hair": {
+    "type": "visible",
+    "frequency": 2,
+    "tier": "rare",
+    "value": 7
+  }
+};
 
