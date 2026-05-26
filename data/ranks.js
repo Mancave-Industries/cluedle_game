35afcd65c30c9b3fad0f775a740149d5e778af54
Mancave-Industries <markymid@me.com>
2026-05-26 17:15:21 +0100

    Update ranks.js

--- data/ranks.js
+++ data/ranks.js
@@ -1,1 +1,68 @@
+window.CLUEDLE_DATA = window.CLUEDLE_DATA || {};
 
+window.CLUEDLE_DATA.ranks = [
+ {
+   id: "trainee_detective",
+   name: "Trainee Detective",
+   guessLimit: 8,
+   dossierStyle: "basic",
+   promotionTarget: 3,
+   demotionTrigger: 2
+ },
+ {
+   id: "detective",
+   name: "Detective",
+   guessLimit: 8,
+   dossierStyle: "standard",
+   promotionTarget: 5,
+   demotionTrigger: 2
+ },
+ {
+   id: "senior_detective",
+   name: "Senior Detective",
+   guessLimit: 7,
+   dossierStyle: "enhanced",
+   promotionTarget: 7,
+   demotionTrigger: 2
+ },
+ {
+   id: "lead_detective",
+   name: "Lead Detective",
+   guessLimit: 7,
+   dossierStyle: "casefile",
+   promotionTarget: 10,
+   demotionTrigger: 2
+ },
+ {
+   id: "detective_lieutenant",
+   name: "Detective Lieutenant",
+   guessLimit: 6,
+   dossierStyle: "classified",
+   promotionTarget: 12,
+   demotionTrigger: 2
+ },
+ {
+   id: "detective_captain",
+   name: "Detective Captain",
+   guessLimit: 6,
+   dossierStyle: "priority",
+   promotionTarget: 15,
+   demotionTrigger: 2
+ },
+ {
+   id: "chief_detective",
+   name: "Chief Detective",
+   guessLimit: 5,
+   dossierStyle: "restricted",
+   promotionTarget: 20,
+   demotionTrigger: 1
+ },
+ {
+   id: "master_detective",
+   name: "Master Detective",
+   guessLimit: 5,
+   dossierStyle: "black_archive",
+   promotionTarget: 25,
+   demotionTrigger: 1
+ }
+];
